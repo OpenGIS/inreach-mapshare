6480148d55a14fc66acf7ee03f2cf869262bdb8e
Joe Hawes <morehawes@gmail.com>
2022-05-11 11:39:21 -0700

    Initial Commit

---
+++ Feed_Beast.php
@@ -0,0 +1,14 @@
+<?php
+	
+/*
+Plugin Name: Feed Beast
+Plugin URI: https://www.morehawes.co.uk/
+Description: Got to feed the beast!
+Version: 1.0
+Text Domain: feed-beast
+Author: Joe Hawes
+Author URI: https://www.morehawes.co.uk/
+*/
+
+require_once('inc/Inreach/Front.php');
+require_once('inc/Inreach/Admin.php');
---
+++ inc/Inreach/Front.php
@@ -0,0 +1,39 @@
+<?php
+
+class Feed_Beast_Inreach_Front {
+	
+	function __construct() {
+		add_shortcode('Feed_Beast', array($this, 'shortcode_handler'));
+	}
+	
+	function shortcode_handler($attributes) {
+		require_once('Inreach_KML_Request.php');
+
+		$attributes = shortcode_atts(array(
+			'mapshare_identifier' => false,
+			'mapshare_password' => false,
+		), $attributes, 'feed_beast');
+	
+		if($attributes['mapshare_identifier']) {					
+			echo do_shortcode('[Waymark]');
+		
+			$inReach_KML_Feed = new inReach_KML_Feed($attributes);
+			$response_geojson_string = $inReach_KML_Feed->response_geojson();
+		
+			Waymark_JS::add_call("
+				setTimeout(function() {
+					var waymark_container = jQuery('.waymark-map').first();
+
+					if(typeof waymark_container === 'object') {			
+						var Waymark_Instance = waymark_container.data('Waymark');
+
+						if(typeof Waymark_Instance === 'object') {
+							Waymark_Instance.load_json(" . $response_geojson_string . ", true);
+						}		
+					}	
+				}, 250);
+			");
+		}
+	}
+}
+new Feed_Beast_Inreach_Front;
