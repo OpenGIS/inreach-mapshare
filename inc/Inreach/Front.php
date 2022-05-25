ef8f1e50aa23e755e1f35c41a547f9b6120a5db0
Joe Hawes <morehawes@gmail.com>
2022-05-25 14:20:24 -0700

    Revert "Basic OL integration"
    This reverts commit d4f5caef2949d322bc9df2c8926e493072d5f0c8.

--- inc/Inreach/Front.php
+++ inc/Inreach/Front.php
@@ -11,105 +11,39 @@ class Inreach_Mapshare_Inreach_Front {
 	}
 	
 	function enqueue_scripts() {
-		$plugin_base = plugin_dir_url('') . Inreach_Mapshare_Config::get_item('plugin_slug');
-		
-		//CSS
-		$ol_css_url = $plugin_base . '/assets/openlayers/ol.css';
-	  wp_enqueue_style(Inreach_Mapshare_Config::get_item('plugin_slug') . '-ol', $ol_css_url);
-	  
-	  //JS
-		$ol_js_url = $plugin_base . '/assets/openlayers/ol.js';
-	  wp_enqueue_script(Inreach_Mapshare_Config::get_item('plugin_slug') . '-ol', $ol_js_url);
+		$css_url = plugin_dir_url('') . Inreach_Mapshare_Config::get_item('plugin_slug') . '/inc/Inreach/front.css';
+
+	  wp_enqueue_style(Inreach_Mapshare_Config::get_item('plugin_slug') . '-inreach-front', $css_url);	
 	}
 	
 	function shortcode_handler($attributes) {
-		$out = '';
-		
 		require_once('Inreach_Mapshare_Inreach.php');
 
-		//Get data
 		$attributes = shortcode_atts(array(
 			'mapshare_identifier' => false,
 			'mapshare_password' => false,
 			'mapshare_date_start' => false,
 			'mapshare_date_end' => false
 		), $attributes, Inreach_Mapshare_Config::get_item('shortcode'));
-		
-		//Required	
+	
 		if($attributes['mapshare_identifier']) {					
-			$map_id = 'inreach-mapshare-map-' . md5(json_encode($attributes));
-			
-			//Make request
 			$Inreach_Mapshare_Inreach = new Inreach_Mapshare_Inreach($attributes);		
 			$response_geojson_string = $Inreach_Mapshare_Inreach->response_geojson();
-
-			$out .= '<div id="' . $map_id . '" class="inreach-mapshare-map" style="height:300px"></div>' . "\n";
-	    $out .= '<script type="text/javascript">
-
-const marker = new CircleStyle({
-  radius: 5,
-  fill: null,
-  stroke: new Stroke({color: "red", width: 1}),
-});
-
-const styles = {
-  "Point": new Style({
-    image: image,
-  }),
-  "LineString": new Style({
-    stroke: new Stroke({
-      color: "red",
-      width: 1,
-    }),
-  }),
-};
-
-const styleFunction = function (feature) {
-  return styles[feature.getGeometry().getType()];
-};
-
-const geojsonObject = ' . $response_geojson_string . ';
-
-const vectorSource = new VectorSource({
-  features: new GeoJSON().readFeatures(geojsonObject),
-});
-
-const vectorLayer = new VectorLayer({
-  source: vectorSource,
-  style: styleFunction,
-});
-
-const map = new Map({
-  layers: [
-    new TileLayer({
-      source: new OSM(),
-    }),
-    vectorLayer,
-  ],
-  target: "' . $map_id . '",
-  view: new View({
-    center: [0, 0],
-    zoom: 2,
-  }),
-});
-			</script>' . "\n";
 		
-// 			Waymark_JS::add_call("
-// 				setTimeout(function() {
-// 					var waymark_container = jQuery('.waymark-map').first();
-// 
-// 					if(typeof waymark_container === 'object') {			
-// 						var Waymark_Instance = waymark_container.data('Waymark');
-// 
-// 						if(typeof Waymark_Instance === 'object') {
-// 							Waymark_Instance.load_json(" . $response_geojson_string . ", true);
-// 						}		
-// 					}	
-// 				}, 250);
-// 			");
+			Waymark_JS::add_call("
+				setTimeout(function() {
+					var waymark_container = jQuery('.waymark-map').first();
+
+					if(typeof waymark_container === 'object') {			
+						var Waymark_Instance = waymark_container.data('Waymark');
+
+						if(typeof Waymark_Instance === 'object') {
+							Waymark_Instance.load_json(" . $response_geojson_string . ", true);
+						}		
+					}	
+				}, 250);
+			");
 		}
-	
-		return $out;
 	}
 }
 new Inreach_Mapshare_Inreach_Front;
