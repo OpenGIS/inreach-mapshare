823fbbf8d9bf1e3e6dc259451ba750d9277d8f1f
Joe Hawes <morehawes@gmail.com>
2022-08-02 17:34:16 -0700

    Demo

--- inc/Inreach.php
+++ inc/Inreach.php
@@ -231,12 +231,14 @@ class InMap_Inreach extends Joe_Class {
 				
 				if($Placemark->Point->coordinates) {
 					$class_append = [];
-
-					if(Joe_Log::has('do_demo')) {
+							
+					if(! Joe_Log::has('do_demo')) {
+						$time_ago = Joe_Helper::time_ago(strtotime($Placemark->TimeStamp->when));
+					} else {				
 						$class_append[] = 'inmap-demo';
-					} 
-
-					$time_ago = Joe_Helper::time_ago(strtotime($Placemark->TimeStamp->when));
+						
+						$time_ago = Joe_Helper::time_ago(strtotime($Placemark->TimeStamp->when), strtotime('5/21/2022 11:04:30 PM'));
+					}
 				
 					//Coordinates
 					$coordinates = explode(',', (String)$Placemark->Point->coordinates);																
@@ -248,40 +250,6 @@ class InMap_Inreach extends Joe_Class {
 					
 					$Feature['geometry']['type'] = 'Point';
 					$Feature['geometry']['coordinates'] = $coordinates;
-
-					//Title
-					$title = '[' . ($i + 1) . '/' . $this->point_count . ']';
-					if(isset($Placemark->TimeStamp->when)) {
-						$title .= $time_ago;						
-					}
-					$Feature['properties']['title'] = $title;
-					
-					//Classes
-					//First (oldest)
-					if($i === 0) {
-						$class_append[] = 'inmap-first';
-
-						//*Only* single item
-						if($this->point_count === 1) {
-							$class_append[] = 'inmap-last inmap-active inmap-only';
-						}
-
-
-						//Most recent
-						$Feature['properties']['title'] = '[' . __('First', Joe_Config::get_item('plugin_text_domain')) . ']';
-						$Feature['properties']['title'] .= $time_ago;	
-					//Last - *LATEST*
-					} elseif(
-						//EOF array
-						$i === sizeof($this->KML->Document->Folder->Placemark) - 2
-					) {
-						//Active
-						$class_append[] = 'inmap-last inmap-active';
-
-						//Most recent
-						$Feature['properties']['title'] = '[' . __('Latest', Joe_Config::get_item('plugin_text_domain')) . ']';
-						$Feature['properties']['title'] .= $time_ago;						
-					}					
 
 					//Extended Data?
 					if(isset($Placemark->ExtendedData)) {
@@ -298,18 +266,20 @@ class InMap_Inreach extends Joe_Class {
 
 									//By Key
 									switch($key) {
+										case 'Time' :
+										
+											
+											$extended_data[$key] = $value;																
+											
+											$this->KML->Document->Folder->Placemark[$i]->ExtendedData->Data[$j]->value = 'Demo Time';
+									
 										case 'Id' :
 											$Feature['properties']['id'] = $value;
 
 											$extended_data[$key] = $value;																
 
 											break;
-										case 'Id' :
-											$Feature['properties']['id'] = $value;
 
-											$extended_data[$key] = $value;																
-
-											break;
 										case 'Text' :
 											//Skip empty text
 											if(! empty($value)) {
@@ -327,6 +297,46 @@ class InMap_Inreach extends Joe_Class {
 						}
 					}
 					
+					//Demo Time
+// 					if(isset($extended_data['Time'])) {
+// 					
+// 					}					
+// 										case 'Time' :
+// 											$extended_data[$key] = 'Demo Time';																
+					
+					//Title
+					$title = '[' . ($i + 1) . '/' . $this->point_count . ']';
+					if(isset($Placemark->TimeStamp->when)) {
+						$title .= $time_ago;						
+					}
+					$Feature['properties']['title'] = $title;
+					
+					//Classes
+					//First (oldest)
+					if($i === 0) {
+						$class_append[] = 'inmap-first';
+
+						//*Only* single item
+						if($this->point_count === 1) {
+							$class_append[] = 'inmap-last inmap-active inmap-only';
+						}
+
+						//Most recent
+						$Feature['properties']['title'] = '[' . __('First', Joe_Config::get_item('plugin_text_domain')) . ']';
+						$Feature['properties']['title'] .= $time_ago;	
+					//Last - *LATEST*
+					} elseif(
+						//EOF array
+						$i === sizeof($this->KML->Document->Folder->Placemark) - 2
+					) {
+						//Active
+						$class_append[] = 'inmap-last inmap-active';
+
+						//Most recent
+						$Feature['properties']['title'] = '[' . __('Latest', Joe_Config::get_item('plugin_text_domain')) . ']';
+						$Feature['properties']['title'] .= $time_ago;						
+					}					
+
 					//By event
 					if(isset($extended_data['Event'])) {
 						//Remove periods!
