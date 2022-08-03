3d81483b95187362b96c154fea8e7e54ebcd8215
Joe Hawes <morehawes@gmail.com>
2022-08-03 15:43:26 -0700

    Settings action link

--- inc/Admin.php
+++ inc/Admin.php
@@ -15,7 +15,18 @@ class InMap_Admin extends Joe_Admin {
 			
 		//Actions
 		add_action('admin_init', array($this, 'load_assets'));
+		add_filter('plugin_action_links_' . Joe_Helper::plugin_file_path(), array($this, 'add_action_links'));				
 	}
+	
+	function add_action_links($links) {
+		$links_before = array();
+
+		$links_after = array(
+			'<a href="' . admin_url('options-general.php?page=' . Joe_Helper::slug_prefix('settings', '-')) . '">' . esc_html__('Settings', Joe_Config::get_item('plugin_text_domain')) . '</a>'
+		);				
+		
+		return array_merge($links_before, $links, $links_after);
+	}	
 	
 	function load_assets() {
 		parent::load_assets();
