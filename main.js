43eee37543a6c5f44aa1ecec27c7de123b45f458
Joe Hawes <morehawes@gmail.com>
2024-05-12 13:08:29 -0700

    3

--- main.js
+++ main.js
@@ -1,2 +1,4 @@
-import "./assets/leaflet.css";
-import "./assets/shortcode.css";
+// <link rel="stylesheet" href="/assets/less/shortcode.less" />
+// <link rel="stylesheet" href="/assets/less/admin.less" />
+import "./src/less/shortcode.less";
+import "./src/less/admin.less";
