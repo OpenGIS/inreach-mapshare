d1043c1e06622f138d6d64c229de2f83a135d98c
Joe Hawes <morehawes@gmail.com>
2026-07-06 19:24:27 -0700

    v3.0.2 bump

--- inreach-mapshare.php
+++ inreach-mapshare.php
@@ -4,7 +4,7 @@
 Plugin Name: inReach MapShare
 Plugin URI: https://github.com/morehawes/inreach-mapshare
 Description: Display inReach MapShare data on your WordPress site. Visit the <a href="options-general.php?page=inreach-mapshare-settings">Settings</a> page to create and customise Shortcodes.
-Version: 3.0.0
+Version: 3.0.2
 Text Domain: inreach-mapshare
 Author: Joe Hawes
 Author URI: https://www.morehawes.ca/
@@ -85,8 +85,8 @@ add_action('init', function () {
 				'basemap_url' => 'https://tile.opentopomap.org/{z}/{x}/{y}.png',
 				'basemap_attribution' => '© &lt;a href=&quot;https://www.openstreetmap.org/copyright&quot;&gt;OpenStreetMap&lt;/a&gt; contributors, SRTM | © &lt;a href=&quot;https://opentopomap.org&quot;&gt;OpenTopoMap&lt;/a&gt; (&lt;a href=&quot;https://creativecommons.org/licenses/by-sa/3.0/&quot;&gt;CC-BY-SA&lt;/a&gt;)',
 				'basemap_title' => 'OpenTopoMap',
-			'basemap_opacity' => '0.7',
-			'basemap_maxzoom' => '17',
+				'basemap_opacity' => '0.7',
+				'basemap_maxzoom' => '17',
 				'detail_expanded' => '1',
 			],
 			'colours' => [
